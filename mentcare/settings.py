05a6ae59400e495d694fc10dceb6f0226becb8c8
unknown <matthew.covington@marquette.edu>
2022-02-24 15:06:53 -0600

    adjusted colors of UI

--- mentcare/settings.py
+++ mentcare/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'mentcare_api',
 ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
