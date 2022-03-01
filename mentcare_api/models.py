65a2769a70030877ec434eab2bc8a750eb24fa4f
unknown <matthew.covington@marquette.edu>
2022-02-28 23:17:05 -0600

    removed secret

--- mentcare_api/models.py
+++ mentcare_api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# Model for patient
 class Patient(models.Model):
     PatientName = models.CharField(max_length=256)
     PatientAdmitDate = models.DateField()
@@ -9,7 +9,7 @@ class Patient(models.Model):
     PatientCondition = models.CharField(max_length=256)
     PatientClinician = models.CharField(max_length=256)
     PatientStatus = models.CharField(max_length=256)
-    CreatedOn = models.DateTimeField(default=timezone.now, help_text='Time is in GMT')
+    CreatedOn = models.DateTimeField(default=timezone.now, help_text='Time is in GMT') # time is in GMT by default
     UpdatedOn = models.DateTimeField(default=timezone.now, null=True, blank=True)
     IsActive = models.IntegerField(default=1, blank=True, null=True, help_text='1->Active, 0->Inactive',
                                    choices=((1, 'Active'), (0, 'Inactive')))
